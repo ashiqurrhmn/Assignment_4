b54be34b91d98c588d1431a6c04aaa48d2d1be72
ashiqurrhmn <arakash115@gmail.com>
2026-02-22 17:25:22 +0600

    set function for toggling button

--- srcipt/script.js
+++ srcipt/script.js
@@ -1,3 +1,6 @@
+let interviewList = [];
+let rejectedList = [];
+
 let total = document.getElementById("total-count");
 let interview = document.getElementById("interview-count");
 let rejected = document.getElementById("rejected-count");
@@ -5,8 +8,16 @@ let availableJobs = document.getElementById("available-jobs");
 
 const allCards = document.getElementById("cards");
 
+const mainContainer = document.querySelector("main");
+
 function count(){
     total.innerText = allCards.children.length;
     availableJobs.innerText = `${allCards.children.length} jobs`;
+    interview.innerText = interviewList.length;
+    rejected.innerText = rejectedList.length;
 }
 count();
+
+function toggleStyle(id){
+    console.log('click', id);
+}
